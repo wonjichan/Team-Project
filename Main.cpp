d8a825e300d3bad1aae63de417fecf6532cb6763
wonjichan <144341788+wonjichan@users.noreply.github.com>
2025-09-07 01:15:42 +0900

    9/6일 Main.cpp
    Main()함수에서 Battle 로직 구현 테스트

---
+++ Main.cpp
@@ -0,0 +1,16 @@
+#include "GameManager.h"
+
+int main()
+{
+	GameManager manager;
+	string name; 
+	cout << "캐릭터 이름을 입력하세요: "; 
+	cin >> name; 
+	Character* player = Character::getInstance(name);
+	cout << "캐릭터 " << player->getName() << " 생성완료! " << "레벨:" << player->GetLevel() 
+		<< ",체력:" << player->getHealth() << ",공격력:" << player->getAttack() << endl;
+	manager.generateMonster();
+	cout << "배틀을 시작합니다!" << endl; 
+	manager.Battle(player);
+	return 0; 
+}
